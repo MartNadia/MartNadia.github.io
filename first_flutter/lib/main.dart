11196385e0a186c3efaf420e1f02538c8ec30134
MartNadia <106438278+MartNadia@users.noreply.github.com>
2025-04-08 10:37:18 +0300

    Add files via upload

---
+++ first_flutter/lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Counter App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Counter App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  Color _incrementButtonColor = Colors.blue; 
+  Color _decrementButtonColor = Colors.blue;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      _incrementButtonColor = Colors.green;
+
+      setState(() {
+         _incrementButtonColor = Colors.blue;
+      });
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+      _decrementButtonColor = Colors.red;
+
+      setState(() {
+        _decrementButtonColor = Colors.blue;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have changed the counter this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _decrementCounter,
+            tooltip: 'Decrement',
+            backgroundColor: _decrementButtonColor,
+            child: const Icon(Icons.remove),
+          ),
+          const SizedBox(width: 16),
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            backgroundColor: _incrementButtonColor,
+            child: const Icon(Icons.add),
+          ),
+        ],
+      ),
+    );
+  }
+}
